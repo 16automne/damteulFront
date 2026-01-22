e3537bd05c3614bd7f2edf965a5f60000ee92893
16automne <manybean00@gmail.com>
2026-01-22 18:56:51 +0900

    수정

--- src/layouts/NoTabsBackLayout.js
+++ src/layouts/NoTabsBackLayout.js
@@ -7,14 +7,10 @@ const NoTabsLayout = () => {
   return (
     <div className='bodyParent'>
       <div className='bodyChild'>
-        <h2>헤더가 있는</h2> 
-=======
-    <div>
-      {/* 뒤로가기버튼 헤더가 있는 */}
-      <AllowHeader />
-      
->>>>>>> 92ec50af7a65abd2e7839b4e1213122da025341a:src/layouts/NoTabsBackLayout.js
-      <Outlet />
+        {/* 뒤로가기버튼 헤더가 있는 */}
+        <AllowHeader />
+        
+        <Outlet />
       </div>
     </div>
   );
