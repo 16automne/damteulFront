00a70973b92c10b07d6bacf6ae52481a2ad72c1e
도건 <kmrjsals@gmail.com>
2026-01-22 17:28:53 +0900

    WriteBtn수정

--- src/components/writeBtn/WriteBtn.js
+++ src/components/writeBtn/WriteBtn.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import './style/writeBtn.scss';
+import './styles/writeBtn.css';
 
 function WriteBtn(props) {
 
