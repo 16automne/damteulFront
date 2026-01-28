a580b618871d8abba2f01946488a99c9f00ae41d
kweon <anybil30@gmail.com>
2026-01-28 15:36:14 +0900

    채팅 서식

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,38 +1,17 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const Chat = () => {
   // 전체 안읽음
-  const [chatBtn,setChatBtn] = useState(false);
+  const [chatBtn,setChatBtn] = useState(0);
   
 
   return (
     <main className='chatPageWrap'>
       {/* 제목 */}
-      <h1>채팅</h1>
-      {/* 전체/안읽음 버튼 */}
-      <div className='btnContainer'>
-        <button onClick={()=>setChatBtn(false)} className={!chatBtn&&'btnActive'}>전체</button>
-        <button onClick={()=>setChatBtn(true)} className={chatBtn&&'btnActive'}>안읽음</button>
+      <div className='title3'>
+        <h2>채팅</h2>
       </div>
-      
-      {/* 채팅 목록 */}
-      <ul>
-        <li>
-          <Link to=''>
-            {/* 이미지 */}
-            <div>
-              <img src='' alt='' />
-            </div>
-            {/* 채팅명 */}
-            <div>
 
-            </div>
-
-            {/* 채팅 알림 */}
-            <div></div>
-          </Link>
-        </li>
-      </ul>
 
 
     </main>
