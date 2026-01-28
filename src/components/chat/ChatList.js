d0b1b2895d82141d9682b1ae10247a2272675438
kweon <anybil30@gmail.com>
2026-01-28 18:41:13 +0900

    채팅제작

---
+++ src/components/chat/ChatList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const ChatList = () => {
+  return (
+    <>
+      {/* 여기서부터 컴포넌트 */}
+      <li>
+        <Link to='' title='채팅바로가기'>
+          {/* 제일 최상위 */}
+          <div className='chatParent'>
+            {/* 채팅 컨텐츠 */}
+            <div className='chatContWrap'>
+              <div className='chatCont'>
+                {/* 이미지 */}
+                <div className='chatImg'>
+                  <img src='https://www.dummyimage.com/60x60/000/fff' alt='사용자프로필' />
+                </div>
+
+                {/* 채팅명 */}
+                <div className='chatTxt'>
+                  {/* 채팅 제목 */}
+                  <div>
+                    <h3>채팅방명</h3>
+                    <span>10분전</span>
+                  </div>
+
+                  {/* 최근 채팅 내용 */}
+                  <p>
+                    나에게 그 물건을 넘겨라 닝겐 아님말고
+                  </p>
+                </div>
+              </div>
+              {/* 채팅 알림 뱃지 */}
+              <span className='chatBadge'>1</span>
+            </div>
+          </div>
+        </Link>
+      </li>
+    </>
+  );
+};
+
+export default ChatList;
--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import ChatList from 'components/chat/ChatList';
+import './styles/chat.css';
+
 const Chat = () => {
   // 전체 안읽음
   const [chatBtn,setChatBtn] = useState(0);
@@ -8,12 +11,36 @@ const Chat = () => {
   return (
     <main className='chatPageWrap'>
       {/* 제목 */}
-      <div className='title3'>
-        <h2>채팅</h2>
+      <h2 className='title'>채팅</h2>
+
+
+      {/* 전체/안읽음 버튼 */}
+      <div className='btnContainer'>
+        <button onClick={()=>setChatBtn(0)} className={chatBtn===0?'btnActive':''}>전체</button>
+        <button onClick={()=>setChatBtn(1)} className={chatBtn===1?'btnActive':''}>안읽음</button>
       </div>
-
-
-
+      
+      {/* 채팅 목록 */}
+      <ul className='chatList'>
+        {chatBtn===0?(
+          <>
+            <ChatList />
+            <ChatList />
+            <ChatList />
+            <ChatList />
+            <ChatList />
+            <ChatList />
+          </>
+        )
+        :(
+          <>
+            <ChatList />
+            <ChatList />
+            <ChatList />
+          </>
+        )
+        }
+      </ul>
     </main>
   );
 };
