8a8ad65fe9a2d58ac86866e1dbf2ed4782e30867
16automne <manybean00@gmail.com>
2026-01-29 15:23:39 +0900

    헤더, 네비 수정

--- src/components/nav/HaveNavi.js
+++ src/components/nav/HaveNavi.js
@@ -26,23 +26,25 @@ const HaveNavi = () => {
 
   return (
     <nav className="naviContainer">
-      {navItems.map((item) => ( //navItems(홈, 커뮤, 나눔, 대화, 내정보)를 하나씩 반복
-        <NavLink //누르면 해당 장소로 이동
-          key={item.to} //이름표
-          to={item.to} //클릭시 도착할 목적지
-          className={({ isActive }) => isActive ? "navItem active" : "navItem"}
-        >
-          {({ isActive }) => (
-            <div className='naviWrap'>
-              <div className="navIcon"> {/* 아이콘 */}
-                {/* 내가 선택한 메뉴에 있다면 fillicon보여주고 아니면 그냥icon */}
-                {isActive ? item.activeIcon : item.icon}
+      <div className="naviInner">
+        {navItems.map((item) => ( //navItems(홈, 커뮤, 나눔, 대화, 내정보)를 하나씩 반복
+          <NavLink //누르면 해당 장소로 이동
+            key={item.to} //이름표
+            to={item.to} //클릭시 도착할 목적지
+            className={({ isActive }) => isActive ? "navItem active" : "navItem"}
+          >
+            {({ isActive }) => (
+              <div className='naviWrap'>
+                <div className="navIcon"> {/* 아이콘 */}
+                  {/* 내가 선택한 메뉴에 있다면 fillicon보여주고 아니면 그냥icon */}
+                  {isActive ? item.activeIcon : item.icon}
+                </div>
+                <span className="navLabel">{item.label}</span> {/* 탭 이름 */}
               </div>
-              <span className="navLabel">{item.label}</span> {/* 탭 이름 */}
-            </div>
-          )}
-        </NavLink>
-      ))}
+            )}
+          </NavLink>
+        ))}
+      </div>
     </nav>
   );
 };
